9898f7308139ddfebe77b7204d0ee0075a1ffc1d
Abhishek <abhishek@7Spans-MacBook-Air-2.local>
2024-11-23 01:03:45 +0530

    [IMP] Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React Modal, you can start with replacing me...</h1>
+      {/* TODO: Start */}
+    </div>
+  );
+}
+
+export default App;
